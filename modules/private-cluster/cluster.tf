e2a18e0f9cb9f95024831ac5497f860bf65bb65c
galynapok <83763079+galynapok@users.noreply.github.com>
2022-08-11 23:05:23 +0300

    Update cluster.tf

--- modules/private-cluster/cluster.tf
+++ modules/private-cluster/cluster.tf
@@ -31,7 +31,13 @@ resource "google_container_cluster" "primary" {
   node_locations    = local.node_locations
   cluster_ipv4_cidr = var.cluster_ipv4_cidr
   network           = "projects/${local.network_project_id}/global/networks/${var.network}"
-  notification_config = var.notification_config
+  notification_config {
+    pubsub {
+      enabled = var.notification_config_topic != "" ? true : false
+      topic   = var.notification_config_topic
+      notification_filter = ["SecurityBulletinEvent", "UpgradeEvent"]
+    }
+  }
   dynamic "network_policy" {
     for_each = local.cluster_network_policy
 
